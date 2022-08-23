8bbf4837937b00c9ef3dd0058d2ff9f101de6c04
ji-0o0o0o <tlswldud28@naver.com>
2022-08-23 11:31:14 +0900

    마이페이지 상세페이지 생성

--- src/main/java/com/example/demo/controller/MyPageController.java
+++ src/main/java/com/example/demo/controller/MyPageController.java
@@ -1,9 +1,24 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.ArticlesResponseDto;
+import com.example.demo.service.ArticlesService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/api/mypage")
+@RequiredArgsConstructor
 @RestController
 public class MyPageController {
 
+    private final ArticlesService articlesService;
+
+    //마이페이지 상세페이지
+    @GetMapping("/{articlesId}")
+    public ArticlesResponseDto readMypage(@PathVariable Long articlesId){
+        return articlesService.readMypage(articlesId);
+    }
 
 }
